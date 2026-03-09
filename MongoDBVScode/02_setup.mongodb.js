886643a95b023425a7cff5a5f102fe645c9fff12
Juned Shaikh <sjuneds128@gmail.com>
2026-03-09 22:41:59 +0530

    remove unnecessary folders

--- MongoDBVScode/02_setup.mongodb.js
+++
@@ -1,8 +0,0 @@
-use('ecommerce');
-
-// db.products.find()
-
-// db.products.find().skip(1).limit(1);
-
-// db.products.updateOne({"name": "Wireless Mouse"}, {$set: {price: 5}});
-db.products.find();
